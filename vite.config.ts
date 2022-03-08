5ccef5409b7624df8773214e969c904746d99a91
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2022-03-08 13:29:18 +0800

    update vite config and cargo.toml

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import Icons from 'unplugin-icons/vite'
-import IconsResolver from 'unplugin-icons/resolver'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
@@ -12,13 +10,9 @@ export default defineConfig({
   plugins: [
     vue(),
     WindiCSS(),
-    Icons(),
     Components({
       dirs: ['src/components'],
       resolvers: [
-        IconsResolver({
-          prefix: 'i'
-        }),
         NaiveUiResolver()
       ]
     }),
